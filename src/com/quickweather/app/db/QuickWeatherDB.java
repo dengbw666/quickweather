52738d136a8bfcab9eaae1ec4fd2e6bfc169bb6a
Bowen <dengbw666@outlook.com>
2016-09-30 13:59:07 -0700

    new database

---
+++ src/com/quickweather/app/db/QuickWeatherDB.java
@@ -0,0 +1,131 @@
+package com.quickweather.app.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.quickweather.app.model.City;
+import com.quickweather.app.model.County;
+import com.quickweather.app.model.Province;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class QuickWeatherDB {
+	
+	public static final String DB_NAME = "quick_weather";
+	
+	public static final int Version = 1;
+	
+	public static QuickWeatherDB quickWeatherDB;
+	
+	public SQLiteDatabase db;
+	
+	private QuickWeatherDB(Context context) {
+		QuickWeatherOpenHelper dbHelper = new QuickWeatherOpenHelper(context, 
+				DB_NAME, null, Version);
+		db = dbHelper.getWritableDatabase();
+	}
+	
+	public synchronized static QuickWeatherDB getInstance(Context context) {
+		if (quickWeatherDB == null) {
+			quickWeatherDB = new QuickWeatherDB(context);
+		}
+		return quickWeatherDB;
+	}
+	
+	public void saveProvince(Province province) {
+		if (province != null) {
+			ContentValues values  = new ContentValues();
+			values.put("province_name", province.getProvinceName());
+			values.put("province_code", province.getProvinceCode());
+			db.insert("Province", null, values);
+		}
+	}
+	
+	public List<Province> loadProvinces() {
+		List<Province> list = new ArrayList<Province>();
+		Cursor cursor = db.query("Province", null, null, null, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				Province province = new Province();
+				province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				province.setProvinceName(cursor.getString
+						(cursor.getColumnIndex("province_name")));
+				province.setProvinceCode(cursor.getString
+						(cursor.getColumnIndex("province_code")));
+				list.add(province);
+			} while (cursor.moveToNext());		
+		}
+		if (cursor != null) {
+			cursor.close();
+		}
+		return list;
+	}
+	
+	public void saveCity(City city) {
+		if (city != null) {
+			ContentValues values = new ContentValues();
+			values.put("city_name", city.getCityName());
+			values.put("city_code", city.getCityCode());
+			values.put("province_id", city.getProvinceId());
+			db.insert("City", null, values);			
+		}
+	}
+	
+	public List<City> loadcities(int provinceId) {
+		List<City> list = new ArrayList<City>();
+		Cursor cursor = db.query("City", null, "province_id = ?", 
+				new String[] { String.valueOf(provinceId) } , null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				City city = new City();
+				city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				city.setCityName(cursor.getString
+						(cursor.getColumnIndex("city_name")));
+				city.setCityCode(cursor.getString
+						(cursor.getColumnIndex("city_code")));
+				city.setProvinceId(provinceId);
+				list.add(city);
+			} while (cursor.moveToNext());		
+		}
+		if (cursor != null) {
+			cursor.close();
+		}
+		return list;
+	}
+	
+	public void saveCounty(County county) {
+		if (county != null) {
+			ContentValues values = new ContentValues();
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			values.put("city_id", county.getCityId());
+			db.insert("County", null, values);			
+		}
+	}
+	
+	public List<County> loadcounties(int cityId) {
+		List<County> list = new ArrayList<County>();
+		Cursor cursor = db.query("County", null, "city_id = ?", 
+				new String[] { String.valueOf(cityId) } , null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				County county = new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString
+						(cursor.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString
+						(cursor.getColumnIndex("county_code")));
+				county.setCityId(cityId);
+				list.add(county);
+			} while (cursor.moveToNext());		
+		}
+		if (cursor != null) {
+			cursor.close();
+		}
+		return list;
+	}
+
+}
